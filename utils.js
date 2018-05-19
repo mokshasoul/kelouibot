9b5b2bf81f17b141f2b0f379b7be84682f185a29
Charis-Nicolas Georgiou <cng_it@posteo.net>
2018-05-19 15:40:01 +0200

    properly written utils function

--- utils.js
+++ utils.js
@@ -9,16 +9,25 @@
  * @param command: assume that we get a command like !<command> <suffix*>
  * where suffix is a string containing the second part of the command
  */
-module.exports =
-    function tokenize_command(command) {
-        var tokenized_command = {
-            command: "",
-            suffix: ""
-        }
-        // We use a tmp array to split, slice and map
-        var tmp = command.split(" ")
+function tokenize_command(command) {
+    var tokenized_command = {
+        command: "",
+        suffix: ""
+    }
+    // We use a tmp array to split, slice and map
+    var tmp = command.split(" ")
+
+    if (tmp.length < 2) {
+        tokenized_command.command = tmp[0];
+        return tokenized_command;
+    }else{
         tokenized_command.command = tmp[0].replace("!", "");
         tokenized_command.suffix = tmp.slice(1, tmp.length).reduce((prev, curr) => {
             return prev + " " + curr;
         });
+    }
+    return tokenized_command;
     };
+module.exports = {
+    tokenize_command: tokenize_command,
+};
