0a758aef79421be7d208b1f36d0fec018c4526d5
Charis-Nicolas Georgiou <cng_it@posteo.net>
2018-05-20 01:07:18 +0200

    rewrite youtube module

--- plugins/youtube/youtube.js
+++ plugins/youtube/youtube.js
@@ -1,37 +1,33 @@
-exports.commands = [
-    "youtube"
-]
 // Imports
-const youtube_node = require("youtube-node");
+const Youtube = require("youtube-node");
 const auth = require("../../auth.json");
-const yt_watch_uri = "https://www.youtube.com/watch?v=";
+
+// CONSTANT VARS
+const YT_WATCH_URI = "https://www.youtube.com/watch?v=";
 
 function YouTubeClient() {
     this.RickrollUrl = 'http://www.youtube.com/watch?v=oHg5SJYRHA0';
-    this.ytclient = new youtube_node();
+    this.ytclient = new Youtube();
+    console.log(auth);
     this.ytclient.setKey(auth.youtube_api_key);
     this.ytclient.addParam('type', 'video');
-    this.respond = function (query, channel, client) {
-        this.ytclient.search(query, 1, function (error, result) {
-            if (error) {
-                channel.send("Ti sto poutso, estilamente lathos");
-                channel.send(this.RickrollUrl);
-            } else {
-                if (!result || result.items ||
-                    result.items.length < 1) {
-                    channel.send(yt_watch_uri +
-                        result.items[0].id.videoId);
-                }
-            }
-
-        })
-    }
 };
-
-exports.youtube = {
-    usage: "<search term>",
-    description: "pulls a list of videos from youtube",
-    process: function (client, msg, suffix) {
-        youtube_
-    }
+YouTubeClient.prototype.respond =  function (query, channel, client) {
+    var that = this;
+    console.log(query);
+    this.ytclient.search(query, 1, function (error, result) {
+    if (error) {
+        console.log(error);
+        channel.send("Ti sto poutso, estilamente lathos");
+        channel.send(that.RickrollUrl);
+    } else {
+        if (!result || result.items ||
+            result.items.length < 1) {
+            channel.send(YT_WATCH_URI +
+                result.items[0].id.videoId);
+        }
+    }});
+};
+module.exports = {
+    YouTubeClient: YouTubeClient
 }
